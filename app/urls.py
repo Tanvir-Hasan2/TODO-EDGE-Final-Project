a75f304d255e1e0022987ec8b7beb24576bee6f1
tanvir-hasan <tanvir@rexsystemsbd.com>
2024-12-29 07:49:05 +0600

    add database model with full flow of sigin signup logut

--- app/urls.py
+++ app/urls.py
@@ -5,11 +5,11 @@ from app.views import home , login , signup , add_todo , signout , delete_todo, 
 
 
 urlpatterns = [
-    path('' , home , name='home' ), 
+   path('' , home , name='home' ), 
    path('login/' ,login  , name='login'), 
    path('signup/' , signup ), 
    path('add-todo/' , add_todo ), 
    path('delete-todo/<int:id>' , delete_todo ), 
    path('change-status/<int:id>/<str:status>' , change_todo ), 
-   path('logout/' , signout )
+   path('logout/' , signout ), 
 ]
